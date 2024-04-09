cbd409cdade835531b9828c7e93e145bf810a4a4
kylermurphy <34041071+kylermurphy@users.noreply.github.com>
2024-04-09 16:15:37 -0400

    Density Profile analysis
    New coding for analyzing the density profile from MSIS and multiple spacecraft

---
+++ den_alt_norm.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  9 16:03:23 2024
+
+@author: krmurph1
+"""
+
+# something to create the data set
+# something to plot the delta latitude and longitude
+# something to calculate the msis profiles where the satellites are close
+# something to normalize the data for those dates
+# something to compare the normalized data with the GOCE and CHAMP data
+
+
+
+def gen_sat_data(sat=sat,
+                 sdate,
+                 edate,
+                 t_tol,
+                 t_back,
+                 t_tot,
+                 cols=['DateTime', 'alt','lat','lon','dens_x']):
+    
+    print('a')
+    
+    
+    
+    
+    
+def gen_msis_profiles():
+    
+    print('a')
+    
+    
+def den_norm():
+    
+    print('a')
